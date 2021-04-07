377ae76261f93f79d31ad232a11f99654e4b21f2
Dhruv Roy Talukdar <dhruvroy8@gmail.com>
2021-04-07 18:01:42 +0530

    backend almost completed

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,7 @@ from app import socketio
 from .models import *
 from app import db
 
+users = {"123456abcderfghi"}
 
 @app.route('/',methods=['POST','GET'])
 def session():
@@ -16,11 +17,15 @@ def session():
 def event_handler(json,methods = ['POST','GET']):
     print('Received message : ',str(json))
 
-    db.session.add(Message(sender=json['user_name'],message_content=json['message'],room_id=Room.query.filter_by(room_key=json['room']).first().room_id))
+    m = Message(sender=json['user_name'],message_content=json['message'],room_id=Room.query.filter_by(room_key=json['room']).first().room_id)
+    db.session.add(m)
     db.session.commit()
 
+    room = Room.query.filter_by(room_key=json['room']).first()
+
+
     if('user_name' in json.keys()):
-        emit('display-client',{"user_name":json['user_name'],"message":json['message'],"roomcode":json['room']},room=json['room'])
+        emit('display-client',{"user_name":json['user_name'],"message":json['message'],"roomcode":json['room'],"id":m.message_id,"members":room.members},room=json['room'])
 
 
 
@@ -29,20 +34,24 @@ def check_room_handler(json):
     print(str(json))
     if db.session.query(Room).filter_by(room_key=json['room_id']).first() == None :
         print("False")
-        emit('if-room',{'present':False,'user':json['username']})
+        emit('if-room',{'present':False,'user':json['username'],'error':'Room with this code does not exits'})
+    elif json['username'] in users:
+        print('False duplicate username')
+        emit('if-room',{'present':False,'user':json['username'],'error':'This user already exists.Try another name.'})    
     else:
         print("True")
-
+        users.add(json['username'])
         room = Room.query.filter_by(room_key=json['room_id']).first()
         list = []
         for messages in Message.query.filter_by(room_id=room.room_id).all():
-            list.append({'user':messages.sender,'message':messages.message_content})
-        emit('if-room',{'present':True,'user':json['username'],'message_list':list})
+            list.append({'user':messages.sender,'message':messages.message_content,'id':messages.message_id})
+        emit('if-room',{'present':True,'user':json['username'],'message_list':list,"members":room.members+1})
 
 
 @socketio.on('join')
 def join_handler(json):
     print(str(json),"join")
+    room = ""
     if 'roomcode' in json.keys():
         username = json['username']
         roomID = json['roomcode']
@@ -56,11 +65,19 @@ def join_handler(json):
         #list_of_rooms.append(roomID)
         db.session.add(Room(room_key=roomID,members=1))
         db.session.commit()
+        users.add(json['username'])
         socketio.emit('add-new-roomcode',{
             'code':roomID
         });
     join_room(roomID)
-    emit("display-client",{"user_name":username,"message":"has entered the room","roomcode":roomID},room=roomID)
+    room = Room.query.filter_by(room_key=roomID).first()
+    emit("display-client",{"user_name":username,"message":"has entered the room","roomcode":roomID,"id":"temp","members":room.members},room=roomID)
+
+@socketio.on('delete-message')
+def delete_message_handler(json):
+    Message.query.filter_by(message_id=int(json["id"])).delete()
+    db.session.commit()
+    emit("remove-message",{"id":json["id"]},room=json["room"])
 
 
 @socketio.on('leave')
@@ -68,11 +85,12 @@ def leave_room_handler(json):
     print(str(json),"leave")
     room = db.session.query(Room).filter_by(room_key=json['room']).first()
     room.members -= 1
+    users.remove(json['username'])
     print("Room :",room)
     if room.members == 0:
         Room.query.filter_by(room_id=room.room_id).delete()
     db.session.commit()   
     if room.members != 0:
-        emit("display-client",{"user_name":json['username'],"message":"has left the room"},room=json['room'])
+        emit("display-client",{"user_name":json['username'],"message":"has left the room","members":room.members,"id":"temp"},room=json['room'])
     leave_room(json['room'])
    
--- app/static/index.js
+++ app/static/index.js
@@ -4,7 +4,7 @@ const form = document.getElementById('form');
 var username = undefined;
 var roomcode = undefined;
 const container = document.getElementById('message-container');
-
+const member_count_id = document.getElementById("member_count_id");
 
 // When you have room code
 form.onsubmit = (e) => {
@@ -30,14 +30,20 @@ form.onsubmit = (e) => {
                     username:user,
                     roomcode:room
                 });
-                appendPreviousMessages(json['message_list']);
                 username = user;
                 roomcode = room;
+
+                // Add previous messages into the client
+                appendPreviousMessages(json['message_list']);
+                
+                // set number of members in the room currently 
+                member_count_id.innerHTML = json['members'].toString();
+                
                 document.getElementById("login-form").classList.add("hide-class");
                 document.getElementById("message-section").classList.remove("hide-class");
                 document.getElementById("greetings").innerHTML = `Welcome ${user} to chat room ${room}!!`;
             }else{
-                alert('There is no room with this id');
+                alert(json['error']);
                 document.getElementById("username").value = "";
                 document.getElementById("roomcode").value = "";
                 console.log('not in list');
@@ -50,11 +56,18 @@ form.onsubmit = (e) => {
 const appendPreviousMessages = (messages) => {
     container.innerHTML = "";
     for(let i=0;i<messages.length;i++){
-        var div = document.createElement('div');
-        div.innerHTML = `<strong> ${messages[i].user} </strong>: ${messages[i].message}`;
+        console.log(messages[i].user);
+        if(username != messages[i].user){   
+            var div = document.createElement('div');
+            div.innerHTML = `<strong> ${messages[i].user} </strong>: ${messages[i].message}`;
+        }else{
+            var div = document.createElement('div');
+            div.innerHTML = `${messages[i].message} :<strong> ${messages[i].user} </strong>`;
+            div.classList.add('mymessages');
+        }
+        div.onclick = handleDeleteFunction;
         container.appendChild(div);
     }
-    //console.log(messages);
 }
 
 // Collect message and pass to server
@@ -81,16 +94,55 @@ socketio.on('display-client',(data) => {
         roomcode = data.roomcode;
         document.getElementById("greetings").innerHTML = `Welcome ${username} to chat room ${roomcode}!!`;
     }
-    var div = document.createElement('div');
-    div.innerHTML = `<strong> ${data.user_name} </strong>: ${data.message}`;
+    
+    // Check if the data has members properties
+    if(data.hasOwnProperty('members')){
+        member_count_id.innerHTML = data['members'].toString();
+    }
+
+    if(username != data.user_name){   
+        var div = document.createElement('div');
+        div.innerHTML = `<strong> ${data.user_name} </strong>: ${data.message}`;
+    }else{
+        var div = document.createElement('div');
+        div.innerHTML = `${data.message} :<strong> ${data.user_name} </strong>`;
+        div.classList.add('mymessages');
+    }
+    div.setAttribute("id",data.id.toString());
+    div.onclick = handleDeleteFunction;
     container.appendChild(div);
 });
+
+// When you want to delete a message
+const handleDeleteFunction = (e) => {
+    var parentdiv;
+    if(e.srcElement.parentNode == container)
+        parentdiv = e.srcElement;
+    else
+        parentdiv = e.srcElement.parentNode;
+    console.log(parentdiv);
+    if(parentdiv.classList.contains("mymessages") && parentdiv.getAttribute("id") !== "temp"){
+        if(confirm("Do you really want to delete your message?")){
+            socketio.emit("delete-message",{
+                id:parentdiv.getAttribute("id"),
+                room:roomcode
+            })
+        }
+    }
+}
+
+// Remove message from he html page
+socketio.on("remove-message",json => {
+    var div = document.getElementById(json["id"])
+    container.removeChild(div);
+})
 
 
 // When you generate new room
 document.getElementById('create-room').onclick = () => {
     var username_input = document.getElementById("username")
     if(username_input.value.length != 0){
+        
         // generate a new code request to the server
         socketio.emit('join',{
             username:username_input.value,
@@ -100,7 +152,7 @@ document.getElementById('create-room').onclick = () => {
         socketio.on('add-new-roomcode',(data) => {
             roomcode = data['room']
         });
-
+        
         username = username_input.value;
         document.getElementById("greetings").innerHTML = `Welcome ${username} to chat room ${roomcode}!!`;
         document.getElementById("login-form").classList.add("hide-class");
