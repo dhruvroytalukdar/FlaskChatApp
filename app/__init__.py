e4cde575d5fb3e25467849f85534a04853f87f93
Dhruv Roy Talukdar <dhruvroy8@gmail.com>
2021-04-20 00:01:18 +0530

    heroku db

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5646addsknc'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:dhruv@2002@localhost/flask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://ba9f0d1576fdaa:29f3f33b@us-cdbr-east-03.cleardb.com/heroku_8f891a807e5ca78'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 migrate = Migrate()
 
