bac80866c49cd851f1112e578c4f8b06654d2a0c
dhruvroytalukdar <dhruvroy8@gmail.com>
2022-08-19 21:51:21 +0530

    moved to sqlite database

--- app/__init__.py
+++ app/__init__.py
@@ -6,14 +6,14 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5646addsknc'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:dhruv@2002@localhost/flask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.sqlite'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://b54626acf43cea:d298619a@us-cdbr-east-03.cleardb.com/heroku_f4837d222bcfda3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 migrate = Migrate()
 
 
 db = SQLAlchemy(app)
-migrate.init_app(app,db)
+migrate.init_app(app, db)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 from app import routes
--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from app import app
 from app import socketio
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, port=8896, debug=True)
